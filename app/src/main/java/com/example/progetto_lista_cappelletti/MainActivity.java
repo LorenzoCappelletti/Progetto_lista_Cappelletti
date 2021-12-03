37056e1dbe26f967a1f73030a6a570574dd36dfc
lorenzo <lorycappe2003@gmail.com>
2021-12-03 14:43:56 +0100

    creazione ArrayAdapter

--- app/src/main/java/com/example/progetto_lista_cappelletti/MainActivity.java
+++ app/src/main/java/com/example/progetto_lista_cappelletti/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.progetto_lista_cappelletti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    ListView listas;
+    String aStati[] = {"Italia" , "Francia" , "Germania" , "Svizzera"};
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listas = (ListView) findViewById(R.id.ListaStati);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this, R.layout.activity_main, R.id.ListaStati, aStati); // contesto corrente è il metodo this. Tutto questo permette di collegare  l'array con la listView
+        listas.setAdapter(aa); // legare la ListView con il nostro ArrayAdapter
+
+
     }
 }
